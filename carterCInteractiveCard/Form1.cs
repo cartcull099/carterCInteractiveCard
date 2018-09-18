7d22f8223b1f0d764f5e43afb049cda8f2d3be7a
cartcull099 <cartcull099@ed.amdsb.ca>
2018-09-18 13:54:08 -0400

    Add project files.

---
+++ carterCInteractiveCard/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace carterCInteractiveCard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Graphics g = this.CreateGraphics();
+            Font drawFont = new Font("Arial", 65, FontStyle.Bold);
+            SolidBrush drawBrush = new SolidBrush(Color.Red);
+            g.DrawString("Happy Birthday!", drawFont, drawBrush, 50, 40);
+
+
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
